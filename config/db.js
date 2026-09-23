6aa6589a081962c6a3cb6247236fa8431973658d
Mohan <monukashyap7895@gmail.com>
2026-09-23 10:03:34 +0530

    frontend-mohan

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    const mongoUri = "mongodb+srv://bravodavid7895_db_user:LdSMeDz3cZf2qQJt@cluster0.taylpfk.mongodb.net/WareHouse";
+    const mongoUri = "mongodb+srv://bravodavid7895_db_user:pP318s9g75kzQz8g@cluster0.m8guqdj.mongodb.net/warehouse";
     if (!mongoUri) {
       throw new Error('Missing MongoDB URI. Set MONGO_URI or MONGODB_URI in your .env file.');
     }
